c92ec7ff62fd2e3f751cb5242bb5b4ce9f6fe9dc
JRizzle1985 <joshuaroode@gmail.com>
2026-08-21 00:56:04 +0200

    Suppress public host session cookies safely

--- app/Http/Middleware/StartSessionUnlessPublicProduct.php
+++ app/Http/Middleware/StartSessionUnlessPublicProduct.php
@@ -3,17 +3,29 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
 use Illuminate\Session\Middleware\StartSession;
+use Symfony\Component\HttpFoundation\Response;
 
 class StartSessionUnlessPublicProduct extends StartSession
 {
+    private bool $suppressCookie = false;
+
     public function handle($request, Closure $next)
     {
-        if ($request instanceof Request && $request->getHost() === config('app.public_product_host')) {
-            return $next($request);
-        }
+        $this->suppressCookie = $request instanceof Request
+            && $request->getHost() === config('app.public_product_host');
 
         return parent::handle($request, $next);
+    }
+
+    protected function addCookieToResponse(Response $response, Session $session)
+    {
+        if ($this->suppressCookie) {
+            return;
+        }
+
+        parent::addCookieToResponse($response, $session);
     }
 }
